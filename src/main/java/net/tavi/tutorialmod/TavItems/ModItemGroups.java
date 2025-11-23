a94a3ac43ed74a8d3a18b421638344ae1e6ffbad
Tavi <nataviussmith@gmail.com>
2025-11-22 21:36:08 -0500

    Added Class for Items

---
+++ src/main/java/net/tavi/tutorialmod/TavItems/ModItemGroups.java
@@ -0,0 +1,9 @@
+package net.tavi.tutorialmod.TavItems;
+
+import net.tavi.tutorialmod.TutorialMod;
+
+public class ModItemGroups {
+    public static void registeritemgroups (){
+        TutorialMod.LOGGER.info("Registering Item Groups for " + TutorialMod.MOD_ID);
+    }
+}
